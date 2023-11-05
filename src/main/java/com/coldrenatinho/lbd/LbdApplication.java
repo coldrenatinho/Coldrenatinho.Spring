80319ccdef00bfa9c97fc9af693ef8799b4708a0
Coldrenatinho <araujorenato045@gmail.com>
2023-11-05 17:02:31 -0400

    fist commit

---
+++ src/main/java/com/coldrenatinho/lbd/LbdApplication.java
@@ -0,0 +1,13 @@
+package com.coldrenatinho.lbd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LbdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LbdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/coldrenatinho/lbd/LbdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coldrenatinho.lbd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LbdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
